ceee3495b842107d7a035320f62845d00bcc99a4
Dalip01 <152367082+Dalip01@users.noreply.github.com>
2023-12-20 19:55:12 +0000

    Data Types

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,8 @@
+console.log("Tech Websiter");
+const accountId = 2324
+const accountName = "Tech Websiter"
+let accountEmail = "Tech@gmail.com"
+var accountPassword = "Techwebsiter"
+accountCity="Chandigarh"
+console.log(accountId);
+console.table([accountId,accountName,accountEmail,accountPassword])
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,10 @@
+"use strict";
+let name = "Dalip" //String type
+let age = 23 //Integer
+let isLoggedIn = true //Boolean
+let state ; // undefined
+let city = null ; //Null
+
+
+console.log(typeof age);
+console.log(typeof null); // Null
