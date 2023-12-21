90e16f2e82d907581d1c6c320637e59759b577cf
Dalip01 <152367082+Dalip01@users.noreply.github.com>
2023-12-21 11:43:44 +0000

    DataTypes in Javascript

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -1,10 +1,31 @@
+//Primitive
+// Types : String, Number, Boolean, null, undefined, Symbol , BigInt
 "use strict";
-let name = "Dalip" //String type
-let age = 23 //Integer
-let isLoggedIn = true //Boolean
-let state ; // undefined
-let city = null ; //Null
+let name = "Dalip"                           //String type
+let age = 23                                //Number
+let isLoggedIn = true                      //Boolean
+let city = null ;                         //Null
+let state ;                              //undefined
+const id = Symbol('123')                //Symbol
+const bigNumber = 34567654324565432345n//BigNumber
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+
 
 
 console.log(typeof age);
-console.log(typeof null); // Null
+console.log(typeof null); // Output : Object
+
+//Reference (Non Primitive)
+//Array,Objects ,Functions
+
+const heros = ["Shaktiman","naagraj","doga"]
+let myObj = {
+    name:"hitesh",
+    age:33,
+}
+const myFunction = function(){
+    console.log("Hello world"); 
+}
+
