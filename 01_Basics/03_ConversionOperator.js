6186047708522fea8ef837e1e426a41b7632b6b0
Dalip01 <152367082+Dalip01@users.noreply.github.com>
2023-12-21 07:46:11 +0000

    DataType Conversion ( In Numbers )

---
+++ 01_Basics/03_ConversionOperator.js
@@ -0,0 +1,64 @@
+let score = 33 
+console.log(typeof(score)) //method to find datatype of any declared variable .
+
+console.log("\n")
+let var1= "44"
+console.log("var1=>",var1)
+console.log(typeof(var1))
+// converting String datatype into Number 
+let stringToNumber = Number(var1)
+console.log(typeof(stringToNumber))
+console.log("String To Number ",stringToNumber)
+
+
+console.log("\n")
+let var2 = null
+console.log("var2=>",var2)
+console.log(typeof(var2))
+
+let nullToNumber = Number(var2)
+console.log(typeof(nullToNumber))
+console.log("Null To Number :",nullToNumber)
+
+
+console.log("\n")
+let var3= undefined
+console.log("var3=>",var3)
+console.log(typeof(var3))
+
+let undefinedToNumber = Number(var2)
+console.log(typeof(undefinedToNumber))
+console.log("Undefined TO Number :",undefinedToNumber)
+console.log("\n")
+
+
+let var4 = ""
+console.log("var4=>",var4)
+console.log(typeof(var4))
+
+let emptyStringToNumber = Number(var2)
+console.log(typeof(emptyStringToNumber))
+console.log("Empty String To Number :",emptyStringToNumber)
+console.log("\n")
+
+
+let var5 = "45abc"
+console.log("var5=>",var5)
+console.log(typeof(var5))
+
+let StringToNumber = Number(var5)
+console.log(typeof(StringToNumber))
+console.log("String To Number :",StringToNumber)
+console.log("\n")
+
+let var6 = true 
+console.log("var6=>",var6)
+console.log(typeof(var6))
+
+let booleanToNumber = Number(var6)
+console.log(typeof(booleanToNumber))
+console.log("Boolean To String :",booleanToNumber)
+console.log("\n")
+
+
+
