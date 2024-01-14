00b70226a132174a9fa41d0d8d931580f712f364
Dalip01 <152367082+Dalip01@users.noreply.github.com>
2024-01-14 08:36:28 +0000

    Immediate Invoked function Execution

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expression (IIFE)
+// In IIFE we have two set of parenthesis ()() , one for declaration and for calling that function 
+
+//Named IIFE - function have some name 
+(function chai(){ 
+    console.log(`DB connected`)
+})(); //semicolon is required to end invoked function.
+
+//Simple IIFE - function don't have any name 
+( (name)=>{
+    console.log(`2.DB connected ${name}`);
+} )
+('dalip') //calling function
