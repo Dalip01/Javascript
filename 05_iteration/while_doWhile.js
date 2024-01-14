544690017beedad36f215009ec974e7f14db0a79
Dalip01 <152367082+Dalip01@users.noreply.github.com>
2024-01-14 19:11:07 +0000

    while and do while

---
+++ 05_iteration/while_doWhile.js
@@ -0,0 +1,18 @@
+let index = 0 
+while (index <=10 ){
+    console.log(`Value of index is ${index}`)
+    index = index + 2 
+}
+let myArray = [ "Loki ", "IronMan " , " Captain America ", "Thor"] 
+let arr = 0 
+while (arr<myArray.length) {
+    console.log(`Array value is ${myArray[arr]}`)
+    arr = arr + 1 
+}
+
+let score = 1 
+do{
+console.log(`score is ${score}`)
+score = score + 1 ;
+}
+while(score<=10)
