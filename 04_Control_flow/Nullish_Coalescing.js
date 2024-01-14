8125e826b913b649c76885352647b23ddb1ef695
Dalip01 <152367082+Dalip01@users.noreply.github.com>
2024-01-14 10:39:20 +0000

    Nullish Coalescing and Ternary Operator

---
+++ 04_Control_flow/Nullish_Coalescing.js
@@ -0,0 +1,15 @@
+let val1 ;
+val1 = 5 ?? 10
+val2 = null ?? 23
+val3 = undefined ?? 233
+
+console.log(val1);
+console.log(val2);
+console.log(val3)
+
+
+// ternary Operator
+
+// condition ? true : false
+const ice = 100
+ice>=80?console.log("greater than 80 "):console.log("not greater than 80")
